60d2cfc70c3cae68eda0a4e9dff1088d61f62416
koya <fukushi098@gmail.com>
2015-02-27 01:42:29 +0900

    Close #6

--- take-thumbnail.py
+++ take-thumbnail.py
@@ -2,14 +2,18 @@ from pymongo import MongoClient
 import requests
 import re
 import sys
-
-g_save_path = "./thumbnails/"
+from pprint import pprint
+from bs4 import BeautifulSoup
 
 DB_CONFIG = {
 	"host" : "localhost",
 	"port" : 27017,
 	"db"   : "nudele"
 }
+
+g_save_path = "./thumbnails/"
+session = None
+Videos = None
 
 def saveImage(url, _id):
 	with open(g_save_path+_id,'wb') as f:
@@ -22,33 +26,68 @@ def connectDB():
 	Videos = db['video']
 	return Videos
 
-def make_path(path):
+def make_saving_path(path):
 	if not re.match(r"/$", path):
 		path = path + '/'
 	return path + 'thumbnails/'
 
+def fc2(video):
+	global Videos
+	_id = str(video['_id'])
+	video_url = video["url"]
+	request = session.get(video_url)
+	soup = BeautifulSoup(request.text)
+	title_tag = soup.find("h2",{"class":["cont_v2_hmenu04", "clearfix"]})
+	if title_tag is not None and title_tag.string is not None and not '(Removed)' in title_tag.string:
+		r = re.compile("changeThumbnail\('(.+\.jpg)',")
+		m = r.search(request.text)
+		image_url = m.group(1)
+		saveImage(image_url, _id)
+		print "saved : "+video_url
+	else:
+		removeVideo(video)
+
+def xvideos(video):
+	global Videos
+	videoId = video['url'].split('/')[-1]
+	_id = str(video['_id'])
+	r = requests.get('http://api.erodouga-rin.net/thumbnails?url=http://jp.xvideos.com/video'+videoId+'/')
+	try:
+		thumbnails = r.json()['thumbnails']
+		saveImage(thumbnails[0], _id)
+		print "saved : "+video["url"]
+	except KeyError:
+		removeVideo(video)
+
+def removeVideo(video):
+	global Videos
+	Videos.remove(video)
+	print "removed ", video["url"]
+
+def loginToFc2():
+	global session
+	if session is None:
+		session = requests.session()
+		login_data = {'email': 'fukushi123@gmail.com', 'pass': 'koya820'}
+		session.post('https://secure.id.fc2.com/index.php?mode=login&switch_language=en', login_data)
+		pprint(session)
+
 def main():
 	global g_save_path
+	global Videos
 	argv = sys.argv
 	if len(argv) == 2:
-		g_save_path = make_path(argv[1])
+		g_save_path = make_saving_path(argv[1])
 
 	Videos = connectDB()
 
+	loginToFc2()
+
 	for video in Videos.find():
 		if video['tag'] == "xvideos":
-			videoId = video['url'].split('/')[-1]
-			_id = str(video['_id'])
-			print 'http://api.erodouga-rin.net/thumbnails?url=http://jp.xvideos.com/video'+videoId+'/'
-			r = requests.get('http://api.erodouga-rin.net/thumbnails?url=http://jp.xvideos.com/video'+videoId+'/')
-			try:
-				thumbnails = r.json()['thumbnails']
-				saveImage(thumbnails[0], _id)
-			except KeyError:
-				#when video was deleted
-				print "removed ", video.get("_id")
-				Videos.remove({"_id":str(video.get("_id"))})
-				continue
+			xvideos(video)
+		if video['tag'] == "fc2":
+			fc2(video)
 
 if __name__ == '__main__':
 	main()
